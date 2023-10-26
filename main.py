3e5ed3eca5b74236f35ee5716b6c33d4f9edc819
robertcoles04 <146145913+robertcoles04@users.noreply.github.com>
2023-10-25 18:51:04 -0700

    encode

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    while true:
+        menu()
+        x = int(input("Please enter an option: "))
+        if x == 1:
+            y = int(input("Please enter your password to encode: "))
+def encode
+
+def menu():
+    print("Menu")
+    print("-" * 13)
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
